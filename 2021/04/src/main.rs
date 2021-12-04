51cdd1dd6ef30e214b65c2e6b434dc8c97af9838
Brad Egler <begler@gmail.com>
2021-12-04 01:23:19 -0500

    Day 4 complete

--- 2021/04/src/main.rs
+++ 2021/04/src/main.rs
@@ -1,4 +1,7 @@
+#![feature(drain_filter)]
+
 use aocf::Aoc;
+use grid::Grid;
 
 const YEAR: i32 = 2021;
 const DAY: u32 = 4;
@@ -28,26 +31,183 @@ fn main() {
     part2(&i);
 }
 
+fn chunks<T>(slice: &[T], mut size: usize) -> impl Iterator<Item = &[T]> {
+    struct ChunksIterator<'a, I> {
+        pub slice: &'a [I],
+        pub total: usize,
+        pub current: usize,
+        pub chunk: usize,
+    }
+    impl<'a, I> Iterator for ChunksIterator<'a, I> {
+        type Item = &'a [I];
+        fn next(&mut self) -> Option<&'a [I]> {
+            if self.current == self.total {
+                None
+            } else if self.slice.len() / self.chunk == 1 && self.slice.len() % self.chunk != 0 {
+                self.current += 2;
+                Some(self.slice)
+            } else {
+                self.current += 1;
+                let (lhs, rhs) = self.slice.split_at(self.chunk);
+                self.slice = rhs;
+                Some(lhs)
+            }
+        }
+    }
+    while slice.len() % size > 1 {
+        size += 1;
+    }
+    let mut len = slice.len() / size;
+    if slice.len() % len != 0 {
+        len += 1;
+    }
+    ChunksIterator {
+        slice,
+        total: len,
+        chunk: size,
+        current: 0,
+    }
+}
+
+struct Board {
+    grid: Vec<Vec<i32>>,
+    state: Grid<bool>,
+}
+
+impl Board {
+    fn is_winner(&self) -> bool {
+        for row in 0..self.state.rows() {
+            if self.state.iter_row(row).all(|&x| x) {
+                return true;
+            }
+        }
+        for col in 0..self.state.cols() {
+            if self.state.iter_col(col).all(|&x| x) {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn call(&mut self, val: i32) {
+        for y in 0..5 {
+            for x in 0..5 {
+                if self.grid[y][x] == val {
+                    self.state[y][x] = true;
+                }
+            }
+        }
+    }
+
+    fn score(&self) -> i32 {
+        let mut score = 0;
+        for y in 0..5 {
+            for x in 0..5 {
+                if !self.state[y][x] {
+                    score += self.grid[y][x];
+                }
+            }
+        }
+        return score;
+    }
+}
+
 fn part1(data: &String) -> i32 {
     println!("Part 1");
-    let input = data
-        .split("\n")
-        .filter(|s| !s.is_empty())
-        .collect::<Vec<&str>>();
+    let input = data.split("\n").collect::<Vec<&str>>();
+    let call_order = input[0]
+        .split(",")
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+    println!("Call order: {:?}", call_order);
+    let mut boards = chunks(&input[1..], 6)
+        .map(|f| {
+            f.iter()
+                .filter(|s| !s.is_empty())
+                .map(|s| s.split_whitespace())
+                .map(|s| s.collect::<Vec<&str>>())
+                .map(|s| {
+                    s.iter()
+                        .map(|v| v.parse::<i32>().unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>()
+        })
+        .map(|f| Board {
+            grid: f,
+            state: Grid::new(5, 5),
+        })
+        .collect::<Vec<_>>();
 
-    let result = input.len();
+    let mut winner_call = -1;
+    let mut winner: Option<&Board> = None;
+    for call in call_order {
+        boards.iter_mut().for_each(|b| b.call(call));
+        winner = boards.iter().filter(|b| b.is_winner()).next();
+        if winner.is_some() {
+            winner_call = call;
+            break;
+        }
+    }
+    println!("Winner call: {}", winner_call);
+    println!("Board: {:?}", winner.unwrap().state);
+    let score = winner.unwrap().score();
+    println!("Score: {}", score);
+
+    let result = score * winner_call;
     println!("Part 1 Result: {}", result);
     return result;
 }
 
 fn part2(data: &String) -> i32 {
     println!("Part 2");
-    let input = data
-        .split("\n")
-        .filter(|s| !s.is_empty())
-        .collect::<Vec<&str>>();
+    let input = data.split("\n").collect::<Vec<&str>>();
+    let call_order = input[0]
+        .split(",")
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+    println!("Call order: {:?}", call_order);
+    let boards = chunks(&input[1..], 6)
+        .map(|f| {
+            f.iter()
+                .filter(|s| !s.is_empty())
+                .map(|s| s.split_whitespace())
+                .map(|s| s.collect::<Vec<&str>>())
+                .map(|s| {
+                    s.iter()
+                        .map(|v| v.parse::<i32>().unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>()
+        })
+        .map(|f| Board {
+            grid: f,
+            state: Grid::new(5, 5),
+        })
+        .collect::<Vec<_>>();
 
-    let result = input.len();
+    let mut winner_call = -1;
+    let mut winner: Option<&mut Board> = None;
+    let mut remaining = boards;
+    for call in call_order {
+        remaining.iter_mut().for_each(|b| b.call(call));
+        let r = remaining
+            .drain_filter(|b| !b.is_winner())
+            .collect::<Vec<Board>>();
+        if r.len() == 0 {
+            winner_call = call;
+            winner = remaining.iter_mut().next();
+            break;
+        }
+        remaining = r;
+    }
+    let winboard = winner.unwrap();
+    println!("Winner call: {}", winner_call);
+    println!("Board: {:?}", winboard.state);
+    let score = winboard.score();
+    println!("Score: {}", score);
+
+    let result = score * winner_call;
     println!("Part 2 Result: {}", result);
     return result;
 }
@@ -60,27 +220,27 @@ mod tests {
     fn test_part1() {
         let data = read_to_string("test.txt").unwrap();
         let count = part1(&data);
-        assert_eq!(198, count);
+        assert_eq!(4512, count);
     }
 
     #[test]
     fn test_part2() {
         let data = read_to_string("test.txt").unwrap();
         let count = part2(&data);
-        assert_eq!(230, count);
+        assert_eq!(1924, count);
     }
 
     #[test]
     fn real_part1() {
         let data = get_input(YEAR, DAY);
         let count = part1(&data);
-        assert_eq!(3813416, count);
+        assert_eq!(31424, count);
     }
 
     #[test]
     fn real_part2() {
         let data = get_input(YEAR, DAY);
         let count = part2(&data);
-        assert_eq!(2990784, count);
+        assert_eq!(23042, count);
     }
 }
