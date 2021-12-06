1becd10f5f3f3aaca5a1a1e10dc02c6c298bc40e
Brad Egler <begler@gmail.com>
2021-12-06 09:14:54 -0500

    2021 day 06 optimizations

--- src/2021/06/main.rs
+++ src/2021/06/main.rs
@@ -9,62 +9,38 @@ fn main() {
     part2(&i);
 }
 
-fn part1(data: &String) -> i32 {
-    println!("Part 1");
-    let start = data
+fn spawn_count(input: &str, days: i32) -> i64 {
+    let start = input
         .split(",")
         .map(|x| x.trim().parse::<i32>().unwrap())
         .collect::<Vec<i32>>();
 
-    let days = 80;
-    let mut fish = start;
-    for range in 1..=days {
-        let mut new_fish = Vec::<i32>::new();
-        fish = fish
-            .iter()
-            .map(|x| {
-                let result = match *x {
-                    0 => {
-                        new_fish.push(8);
-                        6
-                    }
-                    _ => x - 1,
-                };
-                result
-            })
-            .collect::<Vec<i32>>();
-        fish.append(&mut new_fish);
+    let mut fish_days = vec![0; 9];
+    start.iter().for_each(|x| fish_days[*x as usize] += 1);
+    for _ in 1..=days {
+        let mut new_fish_days = vec![0; 9];
+        for i in 0..8 {
+            if i == 0 {
+                new_fish_days[6] = fish_days[i];
+                new_fish_days[8] = fish_days[i];
+            }
+            new_fish_days[i] = new_fish_days[i] + fish_days[i + 1];
+        }
+        fish_days = new_fish_days;
     }
+    fish_days.iter().sum::<i64>()
+}
 
-    let result = fish.len() as i32;
+fn part1(data: &String) -> i64 {
+    println!("Part 1");
+    let result = spawn_count(data, 80);
     println!("Part 1 Result: {}", result);
     return result;
 }
 
 fn part2(data: &String) -> i64 {
     println!("Part 2");
-    let start = data
-        .split(",")
-        .map(|x| x.trim().parse::<i32>().unwrap())
-        .collect::<Vec<i32>>();
-
-    let days = 256;
-    let mut fish_days = vec![0; 9];
-    start.iter().for_each(|x| fish_days[*x as usize] += 1);
-    for _ in 1..=days {
-        let mut new_fish_days = vec![0; 9];
-        new_fish_days[0] = fish_days[1];
-        new_fish_days[1] = fish_days[2];
-        new_fish_days[2] = fish_days[3];
-        new_fish_days[3] = fish_days[4];
-        new_fish_days[4] = fish_days[5];
-        new_fish_days[5] = fish_days[6];
-        new_fish_days[6] = fish_days[0] + fish_days[7];
-        new_fish_days[7] = fish_days[8];
-        new_fish_days[8] = fish_days[0];
-        fish_days = new_fish_days;
-    }
-    let result = fish_days.iter().sum::<i64>();
+    let result = spawn_count(data, 256);
     println!("Part 2 Result: {}", result);
     return result;
 }
@@ -90,6 +66,6 @@ mod tests {
 
     #[test]
     fn t2021_06_rp2() {
-        assert_eq!(17673235392090, part2(&get_input(YEAR, DAY)));
+        assert_eq!(1767323539209, part2(&get_input(YEAR, DAY)));
     }
 }
