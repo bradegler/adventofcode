bae5ec5b05238a63b013708ce639d749f9f1af58
Brad Egler <begler@gmail.com>
2021-12-10 23:33:49 -0500

    Finished 2020 day 07

--- src/2020/07/main.rs
+++ src/2020/07/main.rs
@@ -1,13 +1,19 @@
 use aocshared::get_input;
 use regex::Regex;
+use std::collections::HashMap;
 
 const YEAR: i32 = 2020;
 const DAY: u32 = 07;
+const TARGET_BAG: &str = "shiny gold";
 
 fn main() {
     let i = get_input(YEAR, DAY);
     part1(&i);
     part2(&i);
+}
+
+fn can_hold_bag(bags: &Vec<(i32, String)>, all: &HashMap<String, Vec<(i32, String)>>) -> bool{
+    bags.iter().any(|(_, name)| *name == TARGET_BAG || can_hold_bag(all.get(name).unwrap(), all))
 }
 
 fn part1(data: &String) -> i32 {
@@ -18,30 +24,53 @@ fn part1(data: &String) -> i32 {
         .lines()
         .map(|x| recontainer.captures(x).unwrap())
         .map(|c| (c[1].to_string(), c[2].to_string()))
-        .map(|(b, c)| {
+        .fold(HashMap::new(), |acc, (b, c)| {
+            let mut m = acc;
             if c == "no other bags.".to_string() {
-                (b, vec![])
+                m.insert(b, vec![]);
             } else {
-                (
-                    b,
+                m.insert(b, 
                     recontents
                         .captures_iter(&c)
                         .map(|c| (c[1].parse::<i32>().unwrap(), c[2].to_string()))
                         .collect(),
-                )
+                );
             }
-        })
-        .collect::<Vec<_>>();
+            m
+        });
     println!("{:?}", input);
-
-    let result = 0;
+    let result = input .iter().filter(|(_, v)| can_hold_bag(v, &input)).count();
     println!("Part 1 Result: {}", result);
-    return result;
+    return result.try_into().unwrap();
+}
+
+fn sum_bags(bag_name: &str, bags: &HashMap<String, Vec<(i32, String)>>) -> i32{
+    bags.get(bag_name).unwrap().iter().map(|(count, bag)| count + count * sum_bags(bag, bags)).sum()
 }
 
 fn part2(data: &String) -> i32 {
     println!("Part 2");
-    let result = 0;
+    let recontainer = Regex::new(r"([a-z ]+) bags contain (.*)\.$").unwrap();
+    let recontents = Regex::new(r"(\d+) ([a-z ]+) bags?").unwrap();
+    let input = data
+        .lines()
+        .map(|x| recontainer.captures(x).unwrap())
+        .map(|c| (c[1].to_string(), c[2].to_string()))
+        .fold(HashMap::new(), |acc, (b, c)| {
+            let mut m = acc;
+            if c == "no other bags.".to_string() {
+                m.insert(b, vec![]);
+            } else {
+                m.insert(b, 
+                    recontents
+                        .captures_iter(&c)
+                        .map(|c| (c[1].parse::<i32>().unwrap(), c[2].to_string()))
+                        .collect(),
+                );
+            }
+            m
+        });
+    let result = sum_bags(TARGET_BAG, &input);
     println!("Part 2 Result: {}", result);
     return result;
 }
@@ -57,7 +86,7 @@ mod tests {
 
     #[test]
     fn t2020_07_ep2() {
-        assert_eq!(126, part2(&get_test_input(YEAR, DAY)));
+        assert_eq!(32, part2(&get_test_input(YEAR, DAY)));
     }
 
     #[test]
