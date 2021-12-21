d642f01f81ce95fdc3b71d745fec138d4da748f2
Brad Egler <begler@gmail.com>
2021-12-21 21:14:03 +0000

    2021 part 2

--- src/2021/21/main.rs
+++ src/2021/21/main.rs
@@ -1,4 +1,5 @@
 use aocshared::*;
+use std::collections::HashMap;
 use std::collections::VecDeque;
 
 const YEAR: i32 = 2021;
@@ -52,8 +53,41 @@ fn part1(data: &String) -> i32 {
     scores.iter().min().unwrap() * rolls
 }
 
-fn part2(data: &String) -> i32 {
-    0
+fn part2(data: &String) -> usize {
+    let starts = get_lines_as_strs(data)
+        .iter()
+        .map(|s| s.split_once(": ").unwrap().1.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+    println!("{:?}", starts);
+    let mut pos = HashMap::<[usize; 2], HashMap<[usize; 2], usize>>::new();
+    pos.insert([starts[0], starts[1]], [([0, 0], 1)].into_iter().collect()); // start
+    let mut turn = 0;
+    let mut wins = [0, 0];
+    while !pos.is_empty() {
+        let mut new_pos = HashMap::new();
+        for (p, states) in pos {
+            for (s, f) in [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)] {
+                for (mut score, mut freq) in states.clone() {
+                    let mut p = p.clone();
+                    p[turn] = (p[turn] + s - 1) % 10 + 1;
+                    score[turn] += p[turn];
+                    freq *= f;
+                    if score[turn] >= 21 {
+                        wins[turn] += freq;
+                    } else {
+                        *new_pos
+                            .entry(p)
+                            .or_insert(HashMap::new())
+                            .entry(score)
+                            .or_insert(0) += freq;
+                    }
+                }
+            }
+        }
+        pos = new_pos;
+        turn ^= 1;
+    }
+    *wins.iter().max().unwrap()
 }
 
 #[cfg(test)]
@@ -67,7 +101,7 @@ mod tests {
 
     #[test]
     fn t2021_21_ep2() {
-        assert_eq!(0, part2(&get_test_input(YEAR, DAY)));
+        assert_eq!(444356092776315, part2(&get_test_input(YEAR, DAY)));
     }
 
     #[test]
@@ -77,6 +111,6 @@ mod tests {
 
     #[test]
     fn t2021_21_rp2() {
-        assert_eq!(0, part2(&get_input(YEAR, DAY)));
+        assert_eq!(131180774190079, part2(&get_input(YEAR, DAY)));
     }
 }
