814f847ae4bb63cd80727abb057cfe2ce321751b
Brad Egler <begler@gmail.com>
2022-12-02 00:59:35 -0500

    2022, 02 - cleanup

--- src/2022/02/main.rs
+++ src/2022/02/main.rs
@@ -20,52 +20,55 @@ const DRAW: u64 = 3;
 const LOSE: u64 = 0;
 
 fn part1(data: &String) -> u64 {
+    let rps = HashMap::<char, u64>::from([
+        ('A', ROCK),
+        ('B', PAPER),
+        ('C', SCISSORS),
+        ('X', ROCK),
+        ('Y', PAPER),
+        ('Z', SCISSORS),
+    ]);
 
-    let mut rps = HashMap::new();
-    rps.insert('A', ROCK);
-    rps.insert('B', PAPER);
-    rps.insert('C', SCISSORS);
-    rps.insert('X', ROCK);
-    rps.insert('Y', PAPER);
-    rps.insert('Z', SCISSORS);
-
-    get_lines_as_vec_chars(data).iter().map(|cs| 
-        score_play(*rps.get(&cs[0]).unwrap(), *rps.get(&cs[2]).unwrap())
-    ).sum()
+    get_lines_as_vec_chars(data)
+        .iter()
+        .map(|cs| score_play(*rps.get(&cs[0]).unwrap(), *rps.get(&cs[2]).unwrap()))
+        .sum()
 }
 
 fn score_play(opp: u64, me: u64) -> u64 {
     (match opp {
-        ROCK if me == PAPER=> WIN,
-        ROCK if me == SCISSORS=> LOSE,
-        ROCK if me == ROCK=> DRAW,
+        ROCK if me == PAPER => WIN,
+        ROCK if me == SCISSORS => LOSE,
+        ROCK if me == ROCK => DRAW,
         PAPER if me == PAPER => DRAW,
         PAPER if me == SCISSORS => WIN,
         PAPER if me == ROCK => LOSE,
         SCISSORS if me == PAPER => LOSE,
         SCISSORS if me == SCISSORS => DRAW,
         SCISSORS if me == ROCK => WIN,
-        _ => todo!()
+        _ => todo!(),
     }) + me
 }
 
 fn part2(data: &String) -> u64 {
-    let mut rps = HashMap::new();
-    rps.insert('A', ROCK);
-    rps.insert('B', PAPER);
-    rps.insert('C', SCISSORS);
-    let mut result = HashMap::new();
-    result.insert('X', LOSE);
-    result.insert('Y', DRAW);
-    result.insert('Z', WIN);
+    let rps = HashMap::<char, u64>::from([
+        ('A', ROCK),
+        ('B', PAPER),
+        ('C', SCISSORS),
+        ('X', LOSE),
+        ('Y', DRAW),
+        ('Z', WIN),
+    ]);
 
-    let turns = get_lines_as_vec_chars(data);
-    turns.iter().map(|cs| {
-        let opp = rps.get(&cs[0]).unwrap();
-        let result = result.get(&cs[2]).unwrap();
-        let me = get_for_result(*opp, *result);
-        score_play(*opp, me)
-    }).sum()
+    get_lines_as_vec_chars(data)
+        .iter()
+        .map(|cs| {
+            let opp = rps.get(&cs[0]).unwrap();
+            let result = rps.get(&cs[2]).unwrap();
+            let me = get_for_result(*opp, *result);
+            score_play(*opp, me)
+        })
+        .sum()
 }
 
 fn get_for_result(opp: u64, result: u64) -> u64 {
@@ -79,7 +82,7 @@ fn get_for_result(opp: u64, result: u64) -> u64 {
         SCISSORS if result == WIN => ROCK,
         SCISSORS if result == LOSE => PAPER,
         SCISSORS if result == DRAW => SCISSORS,
-        _ => todo!()
+        _ => todo!(),
     }
 }
 
