e91d49bb6f2908f00e50f7241969e46b09b9f212
Brad Egler <begler@gmail.com>
2025-12-01 07:17:48 -0500

    2024 08

--- src/2024/08/main.rs
+++ src/2024/08/main.rs
@@ -13,21 +13,84 @@ fn main() {
 
 fn part1(data: &String) -> u64 {
     let content = Grid::new(get_lines_as_grid_char(data));
-    let antinodes = vec![vec![0; content.width()]; content.height()];
+    let mut antinodes = Grid::new(vec![vec![0; content.width()]; content.height()]);
 
     let nodes = content
         .into_iter()
         .filter(|(_, _, v)| *v != '.')
+        .map(|(x, y, v)| (x as i32, y as i32, v))
         .collect_vec();
 
-    for node in nodes {
+    for node in nodes.clone() {
         println!("{:?}", node);
+        // find all nodes of the same frequency
+        let same_frequency_nodes = nodes
+            .iter()
+            .filter(|n| n.2 == node.2)
+            .filter(|n| n.0 != node.0 && n.1 != node.1)
+            .collect_vec();
+        println!("same_frequency_nodes: {:?}", same_frequency_nodes);
+        // calculate distancce between nodes
+        let distances = same_frequency_nodes
+            .iter()
+            .map(|n| ((n.0 - node.0), (n.1 - node.1)))
+            .collect_vec();
+        println!("distances: {:?}", distances);
+        // calculate the antinode as 2x distance to node
+        for distance in distances {
+            let antinode = (node.0 + distance.0 * 2, node.1 + distance.1 * 2);
+            println!("antinode: {:?}", antinode);
+            if antinodes.in_bounds(antinode) {
+                antinodes.set((antinode.0 as usize, antinode.1 as usize), 1);
+            }
+        }
     }
-    Grid::new(antinodes).into_iter().map(|(_, _, i)| i).sum()
+    antinodes.into_iter().map(|(_, _, i)| i).sum()
 }
 
 fn part2(data: &String) -> u64 {
-    0
+    let content = Grid::new(get_lines_as_grid_char(data));
+    let mut antinodes = Grid::new(vec![vec![0; content.width()]; content.height()]);
+
+    let nodes = content
+        .into_iter()
+        .filter(|(_, _, v)| *v != '.')
+        .map(|(x, y, v)| (x as i32, y as i32, v))
+        .collect_vec();
+
+    for node in nodes.clone() {
+        println!("{:?}", node);
+        // find all nodes of the same frequency
+        let same_frequency_nodes = nodes
+            .iter()
+            .filter(|n| n.2 == node.2)
+            .filter(|n| n.0 != node.0 && n.1 != node.1)
+            .collect_vec();
+        println!("same_frequency_nodes: {:?}", same_frequency_nodes);
+        // calculate distance between nodes
+        let distances = same_frequency_nodes
+            .iter()
+            .map(|n| ((n.0 - node.0), (n.1 - node.1)))
+            .collect_vec();
+        println!("distances: {:?}", distances);
+        // calculate the antinode as 2x distance to node
+        for distance in distances {
+            antinodes.set(
+                (
+                    node.0 as usize + distance.0 as usize,
+                    node.1 as usize + distance.1 as usize,
+                ),
+                1,
+            );
+            let mut antinode = (node.0 + distance.0 * 2, node.1 + distance.1 * 2);
+            while antinodes.in_bounds(antinode) {
+                println!("antinode: {:?}", antinode);
+                antinodes.set((antinode.0 as usize, antinode.1 as usize), 1);
+                antinode = (antinode.0 + distance.0 * 2, antinode.1 + distance.1 * 2);
+            }
+        }
+    }
+    antinodes.into_iter().map(|(_, _, i)| i).sum()
 }
 
 #[cfg(test)]
@@ -40,16 +103,17 @@ mod tests {
 
     #[test]
     fn t2024_08_ep2() {
-        assert_eq!(0, part2(&get_test_input(YEAR, DAY)));
+        assert_eq!(9, part2(&get_test_input_part(YEAR, DAY, 2)));
+        assert_eq!(34, part1(&get_test_input(YEAR, DAY)));
     }
 
     #[test]
     fn t2024_08_rp1() {
-        assert_eq!(0, part1(&get_input(YEAR, DAY)));
+        assert_eq!(392, part1(&get_input(YEAR, DAY)));
     }
 
-    #[test]
-    fn t2024_08_rp2() {
-        assert_eq!(0, part2(&get_input(YEAR, DAY)));
-    }
+    // #[test]
+    // fn t2024_08_rp2() {
+    //     assert_eq!(0, part2(&get_input(YEAR, DAY)));
+    // }
 }
