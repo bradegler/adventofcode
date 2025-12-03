ff7ff472546b6a7558e263db277ce37e237399ff
Brad Egler <begler@gmail.com>
2025-12-03 08:46:50 -0500

    2025 03

--- src/2025/03/main.rs
+++ src/2025/03/main.rs
@@ -11,59 +11,45 @@ fn main() {
     println!("Part 2: [{}]", part2(&i));
 }
 
-fn scan_bank(bank: &Vec<u32>, start: usize, end: usize) -> (usize, u32) {
-    let mut highest_idx = 0;
-    let mut highest_val = 0;
-    for cidx in start..end {
-        let val = bank[cidx];
-        if val > highest_val {
-            highest_idx = cidx;
-            highest_val = val;
+fn scan_bank(bank: &Vec<u32>, start: usize, end: usize) -> (i32, u32) {
+    (start..end).fold((0 as i32, 0), |acc, cidx| {
+        if bank[cidx] > acc.1 {
+            (cidx as i32, bank[cidx])
+        } else {
+            acc
         }
-    }
-    (highest_idx, highest_val)
+    })
+}
+
+fn calc_total_joltage(data: &String, digit_count: usize) -> u64 {
+    get_lines_as_grid_u32(data)
+        .iter()
+        .map(|bank| {
+            let mut last: (i32, u32) = (-1, 0);
+            (0..digit_count)
+                .map(|scan_idx| {
+                    last = scan_bank(
+                        &bank,
+                        (last.0 + 1) as usize,
+                        bank.len() - (digit_count - 1 - scan_idx),
+                    );
+                    char::from_digit(last.1, 10).unwrap()
+                })
+                .collect_vec()
+                .iter()
+                .collect::<String>()
+                .parse::<u64>()
+                .unwrap()
+        })
+        .sum()
 }
 
 fn part1(data: &String) -> u64 {
-    let banks = get_lines_as_grid_u32(data);
-    let mut total_joltage = 0;
-    for ridx in 0..banks.len() {
-        let bank = &banks[ridx];
-        let first = scan_bank(&bank, 0, bank.len() - 1);
-        let second = scan_bank(&bank, first.0 + 1, bank.len());
-        let bank_joltage = first.1 * 10 + second.1;
-        println!(
-            "Bank {}: {:?} {:?}-{:?} {}",
-            ridx, bank, first, second, bank_joltage
-        );
-        total_joltage += bank_joltage;
-    }
-    total_joltage as u64
+    calc_total_joltage(data, 2)
 }
 
 fn part2(data: &String) -> u64 {
-    let banks = get_lines_as_grid_u32(data);
-    let mut total_joltage: u64 = 0;
-    let joltages = banks
-        .iter()
-        .enumerate()
-        .map(|(ridx, bank)| {
-            let bank_len = bank.len();
-            let mut last = (0, 0);
-            let mut bank_joltage = "".to_owned();
-            for scan_idx in 0..12 {
-                let start = if scan_idx == 0 { 0 } else { last.0 + 1 };
-                let max = scan_bank(&bank, start, bank_len - (11 - scan_idx));
-                last = max;
-                bank_joltage.push_str(&max.1.to_string());
-            }
-            println!("Bank {}: {:?} = {}", ridx, bank, bank_joltage);
-            bank_joltage.parse::<u64>().unwrap()
-        })
-        .collect_vec();
-    total_joltage = joltages.iter().sum();
-    println!("Joltages: {:?} == {:?}", joltages, total_joltage);
-    total_joltage
+    calc_total_joltage(data, 12)
 }
 
 #[cfg(test)]
