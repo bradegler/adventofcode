02bf123ab7855b0daca6a1bb60ced86fe389703f
Brad Egler <begler@gmail.com>
2025-12-13 08:00:58 -0500

    2025 07

--- src/2025/07/main.rs
+++ src/2025/07/main.rs
@@ -1,6 +1,6 @@
 use aocshared::{
     aoc::aoc::*,
-    grid::grid::{Grid, Visitable},
+    grid::grid::{Grid, Point, Visitable},
 };
 
 const YEAR: i32 = 2025;
@@ -56,7 +56,7 @@ fn part1(data: &String) -> u64 {
 }
 
 fn part2(data: &String) -> u64 {
-    let mut grid = Grid::new(get_lines_as_grid_char(data));
+    let grid = Grid::new(get_lines_as_grid_char(data));
     let mut start = 0;
     for col in 0..grid.width() {
         if grid.at((0, col)) == 'S' {
@@ -64,12 +64,29 @@ fn part2(data: &String) -> u64 {
             break;
         }
     }
-    grid.print_row(0);
-    println!();
+    grid.print();
+    let mem = Grid::new(vec![vec![0; grid.width()]; grid.height()]);
+    1 + count_branches(&mut grid.clone(), &mut mem.clone(), (1, start as i32))
+}
 
-    let mut beams = vec![];
-    beams.push((0, start));
-    0
+fn count_branches(grid: &mut Grid<char>, mem: &mut Grid<usize>, beam: Point<i32>) -> u64 {
+    let mut count = 0;
+    if grid.in_bounds(beam) {
+        if grid.at((beam.0 as usize, beam.1 as usize)) == '.' {
+            count = count_branches(grid, mem, (beam.0 + 1, beam.1));
+        } else if grid.at((beam.0 as usize, beam.1 as usize)) == '^' {
+            if !grid.is_visited((beam.0 as usize, beam.1 as usize)) {
+                grid.visit((beam.0 as usize, beam.1 as usize));
+                let left = count_branches(grid, mem, (beam.0, beam.1 - 1));
+                let right = count_branches(grid, mem, (beam.0, beam.1 + 1));
+                count = 1 + left + right;
+                mem.set((beam.0 as usize, beam.1 as usize), count as usize);
+            } else {
+                count = mem.at((beam.0 as usize, beam.1 as usize)) as u64;
+            }
+        }
+    }
+    count
 }
 
 #[cfg(test)]
@@ -90,8 +107,8 @@ mod tests {
         assert_eq!(1609, part1(&get_input(YEAR, DAY)));
     }
 
-    // #[test]
-    // fn t2025_07_rp2() {
-    //     assert_eq!(0, part2(&get_input(YEAR, DAY)));
-    // }
+    #[test]
+    fn t2025_07_rp2() {
+        assert_eq!(12472142047197, part2(&get_input(YEAR, DAY)));
+    }
 }
